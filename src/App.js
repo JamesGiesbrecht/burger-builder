6747eca5b1e97c37a8556b809e73c736026574f9
James Giesbrecht <jagiesbrecht@wawanesa.com>
2020-02-10 14:20:00 -0600

    Creating components and structure

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import Layout from './components/Layout/Layout'
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder'
 
 class App extends Component {
   constructor() {
@@ -11,7 +13,9 @@ class App extends Component {
   render() {
     return (
       <div>
-        
+        <Layout>
+          <BurgerBuilder />
+        </Layout>
       </div>
     )
   }
