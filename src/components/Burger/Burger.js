c0ea247ff37fc41abc4c71d29eda23298f6cb16d
James Giesbrecht <jagiesbrecht@wawanesa.com>
2020-02-10 15:17:04 -0600

    When state contains no ingredients a message will be displayed

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,7 +4,7 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient'
 
 const Burger = (props) => {
     //  Turns object keys (meat, cheese, etc.) into an array
-    const ingredients = Object.keys(props.ingredients)
+    let ingredients = Object.keys(props.ingredients)
         //  Returning an array that is the same length as the values
         // e.g. meat: 2 would return an array with blank spots that 2 spots long
         .map(igKey => {
@@ -14,6 +14,17 @@ const Burger = (props) => {
                 return <BurgerIngredient key={igKey + i} type={igKey} />
             })
         })
+        //  Reduce takes a function with arguments representing the pervious value and the current value
+        //  This will loop through the array and add them to the initial array (which is empty and passed via the second param)
+        //  Now if ingredients are 0 it will be removed from the array
+        .reduce((arr, el) => {
+            return arr.concat(el)
+        //  The empty array as the second param of reduce represents the initial value
+        }, [])
+    
+    if(ingredients.length === 0) {
+        ingredients = <p>Add those ingredients partner!</p>
+    }
 
     return (
         <div className={classes.Burger}>
