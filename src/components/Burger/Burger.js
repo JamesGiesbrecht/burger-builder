1cbb9e9142343b043769ea087eb086658e0f63d8
James Giesbrecht <jagiesbrecht@wawanesa.com>
2020-02-10 15:04:07 -0600

    Tied the Burger Building to state

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,11 +3,22 @@ import classes from './Burger.module.css'
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient'
 
 const Burger = (props) => {
+    //  Turns object keys (meat, cheese, etc.) into an array
+    const ingredients = Object.keys(props.ingredients)
+        //  Returning an array that is the same length as the values
+        // e.g. meat: 2 would return an array with blank spots that 2 spots long
+        .map(igKey => {
+            return [...Array(props.ingredients[igKey])]
+            //  Creates a BurgerIngredient of that type for the array length 
+            .map((_,i) => {
+                return <BurgerIngredient key={igKey + i} type={igKey} />
+            })
+        })
+
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top" />
-            <BurgerIngredient type="cheese" />
-            <BurgerIngredient type="meat" />
+            {ingredients}
             <BurgerIngredient type="bread-bottom" />
         </div>
     )
