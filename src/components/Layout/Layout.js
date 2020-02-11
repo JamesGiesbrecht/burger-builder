86bf6577b493d866cfcfed256075782fd3d2cdfd
James Giesbrecht <jagiesbrecht@wawanesa.com>
2020-02-11 09:02:46 -0600

    Added mobile sidedrawer component with responsive stylings

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import classes from './Layout.module.css'
 import Toolbar from '../Navigation/Toolbar/Toolbar'
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer'
 
 const Layout = (props) => (
     <>
         <Toolbar />
-        <div>Sidedrawer, Backdrop</div>
+        <SideDrawer />
+        <div>Backdrop</div>
         <main className={classes.Content}>
             {props.children}
         </main>
