8923cf4d3ae95263624a0c2d73154ba79e16909a
James Giesbrecht <jagiesbrecht@wawanesa.com>
2020-02-19 14:11:51 -0600

    Dynamically retrieving all orders from firebase and displaying them on the orders page

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -1,11 +1,36 @@
 import React from 'react'
 import classes from './Order.module.css'
 
-const Order = (props) => (
-    <div className={classes.Order}>
-        <p>Ingrdients: </p>
-        <p>Price: <strong>{props.price}</strong></p>
-    </div>
-)
+const Order = (props) => {
+    const ingredients = []
+
+    for (let ingredientName in props.ingredients) {
+        ingredients.push({
+            name: ingredientName,
+            amount: props.ingredients[ingredientName]
+        })
+    }
+    
+    return (
+        <div className={classes.Order}>
+            <p>Ingredients: {ingredients.map(ingredient => (
+                <span 
+                    key={ingredient.name}
+                    style={{
+                        textTransform: 'capitalize',
+                        display: 'inline-block',
+                        margin: '0 8px',
+                        border: '1px solid #ccc',
+                        padding: '5px'
+                    }}
+                >
+                    {ingredient.name} ({ingredient.amount})
+                </span>
+            ))}
+            </p>
+            <p>Price: <strong>${props.price.toFixed(2)}</strong></p>
+        </div>
+    )
+}
 
 export default Order
--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -1,15 +1,45 @@
 import React, { Component } from 'react'
 import Order from '../../components/Order/Order'
+import axios from '../../axios/axios-orders'
+import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler'
 
 class Orders extends Component {
+    state = {
+        orders: [],
+        isLoading: true
+    }
+
+    componentDidMount() {
+        axios.get('/orders.json')
+            .then(response => {
+                const fetchedOrders = []
+                for (let key in response.data) {
+                    //  Taking each order (with the randomly generated key) and spreading its properties into a new object
+                    fetchedOrders.push({
+                        ...response.data[key],
+                        id: key
+                    })
+                }
+                this.setState({
+                    loading: false,
+                    orders: fetchedOrders
+                })
+                console.log(fetchedOrders)
+            })
+            .catch(error => {
+                this.setState({ loading: false })
+            })
+    }
+
     render() {
         return (
             <div>
-                <Order />
-                <Order />
+                {this.state.orders.map(order => (
+                    <Order key={order.id} ingredients={order.ingredients} price={order.price} />
+                ))}
             </div>
         )
     }
 }
 
-export default Orders
+export default withErrorHandler(Orders, axios)
