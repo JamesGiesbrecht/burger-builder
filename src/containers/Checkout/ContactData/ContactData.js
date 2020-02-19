78c994f719ce9ae4cded1292ae6e9e404379fc5e
James Giesbrecht <jagiesbrecht@wawanesa.com>
2020-02-19 14:49:56 -0600

    Created a dynamic Input component and added it to our contact form

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -3,6 +3,7 @@ import Button from '../../../components/UI/Button/Button'
 import classes from './ContactData.module.css'
 import axios from '../../../axios/axios-orders'
 import Spinner from '../../../components/UI/Spinner/Spinner'
+import Input from '../../../components/UI/Input/Input'
 
 class ContactData extends Component {
     state = {
@@ -49,10 +50,30 @@ class ContactData extends Component {
         <>
             <h4>Enter your Contact Data</h4>
             <form>
-                <input type='text' name='name' placeholder='Your Name' />
-                <input type='text' name='email' placeholder='Your Email' />
-                <input type='text' name='street' placeholder='Street' />
-                <input type='text' name='postalCode' placeholder='Postal Code' />
+                <Input
+                    inputtype='input' 
+                    type='text' 
+                    name='name' 
+                    placeholder='Your Name'
+                />
+                <Input
+                    inputtype='input' 
+                    type='text' 
+                    name='email' 
+                    placeholder='Your Email'
+                />
+                <Input
+                    inputtype='input' 
+                    type='text' 
+                    name='street' 
+                    placeholder='Street'
+                />
+                <Input
+                    inputtype='input' 
+                    type='text' 
+                    name='postalCode' 
+                    placeholder='Postal Code'
+                />
                 <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
             </form>
         </>
