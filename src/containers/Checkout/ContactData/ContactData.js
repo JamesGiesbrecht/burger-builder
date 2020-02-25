b92c30e9aa03b714d72441f4c477ed3bf27da907
James Giesbrecht <jagiesbrecht@wawanesa.com>
2020-02-25 11:55:21 -0600

    Filtered orders by the userId

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -154,7 +154,8 @@ class ContactData extends Component {
         const order = {
             ingredients: this.props.ingredients,
             price: this.props.price,
-            orderData: formData
+            orderData: formData,
+            userId: this.props.userId
         }
 
         this.props.onOrderBurger(order, this.props.token)
@@ -206,7 +207,8 @@ const mapStateToProps = state => {
         ingredients: state.burgerBuilder.ingredients,
         price: state.burgerBuilder.price,
         isLoading: state.order.isLoading,
-        token: state.auth.token
+        token: state.auth.token,
+        userId: state.auth.userId
     }
 }
 
--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -9,7 +9,7 @@ import Spinner from '../../components/UI/Spinner/Spinner'
 class Orders extends Component {
 
     componentDidMount() {
-        this.props.onFetchOrders(this.props.token)
+        this.props.onFetchOrders(this.props.token, this.props.userId)
     }
 
     render() {
@@ -29,13 +29,14 @@ const mapStateToProps = state => {
     return {
         orders: state.order.orders,
         isLoading: state.order.isLoading,
-        token: state.auth.token
+        token: state.auth.token,
+        userId: state.auth.userId
     }
 }
 
 const mapDispatchToProps = dispatch => {
     return {
-        onFetchOrders: (token) => dispatch(actionTypes.fetchOrders(token))
+        onFetchOrders: (token, userId) => dispatch(actionTypes.fetchOrders(token, userId))
     }
 }
 
