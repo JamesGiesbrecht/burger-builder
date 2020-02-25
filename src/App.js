b51a4a42c378933c70ea169a9eee780f52891f9c
James Giesbrecht <jagiesbrecht@wawanesa.com>
2020-02-25 11:42:50 -0600

    Added route guarding

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder'
 import Checkout from './containers/Checkout/Checkout'
 import Orders from './containers/Orders/Orders'
 import Auth from './containers/Auth/Auth'
-import { Route, Switch } from 'react-router-dom'
+import { Route, Switch, Redirect, withRouter } from 'react-router-dom'
 import Logout from './containers/Auth/Logout/Logout'
 import { connect } from 'react-redux'
 import * as actionTypes from './store/actions/'
@@ -15,19 +15,42 @@ class App extends Component {
   }
   
   render() {
-    return (
-      <Layout>
-        {/* Switch only loads the first route that matches */}
+    let routes
+    
+    // Dynamically loading routes based on auth status
+    if (this.props.isLoggedIn) {
+      routes = (
         <Switch>
           <Route path='/checkout' component={Checkout} />
           <Route path='/orders' component={Orders} />
-          <Route path='/auth' component={Auth} />
           <Route path='/logout' component={Logout} />
+          <Route path='/' exact component={BurgerBuilder} />
+          <Redirect to='/' />
+        </Switch>
+      )
+    } else {
+      routes = (
+        <Switch>
+          {/* Switch only loads the first route that matches */}
+          <Route path='/auth' component={Auth} />
           {/* Exact only loads this route if it matches the path exactly */}
           <Route path='/' exact component={BurgerBuilder} />
+          <Redirect to='/' />
         </Switch>
+      )
+    }
+
+    return (
+      <Layout>
+        {routes}
       </Layout>
     )
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    isLoggedIn: state.auth.token !== null
   }
 }
 
@@ -37,4 +60,4 @@ const mapDispatchToProps = dispatch => {
   }
 }
 
-export default connect(null, mapDispatchToProps)(App)
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(App))
